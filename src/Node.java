795eec6128148b5121ebd328eb6fd9359337f888
ibrahimAM19 <ibrax1523@gmail.com>
2026-04-30 11:02:23 +0300

    Create Node.java

---
+++ src/Node.java
@@ -0,0 +1,31 @@
+public class Node {
+    private Object data; // Stores any kind of data 
+    private Node next;
+
+    public Node(Object obj) { // Constructor 
+        data = obj;
+        next = null;
+    }
+
+    // Link access methods
+    public void setNext(Node nextPtr) {
+        next = nextPtr;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    // Data access methods
+    public void setData(Object obj) {
+        data = obj;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public String toString() {
+        return data.toString();
+    }
+}
